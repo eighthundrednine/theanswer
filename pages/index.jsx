64ccfa4c94a87e6b2154591d85bc1ea6a071e72e
eighthundrednine <wdkevo@gmail.com>
2022-04-09 22:47:18 -0400

    Took out Zeep quote

--- pages/index.jsx
+++ pages/index.jsx
@@ -14,7 +14,6 @@ const Home = () => {
       "One dot, mothafucka!",
       "Whatever you're asking, the answer is: I'm amazing.",
       "If I let you make me nervous, then we can't get schwifty.",
-      "Eek-barba-derkel! Somebody's gonna get laid in college!",
       "Eek-barba-derkel? That's a pretty fucked up 'ooh-la-la.'"
     ];
     return quotesList[getRandomNumber(quotesList.length)];
