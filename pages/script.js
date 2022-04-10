a6e0fac35ecb80c64fe32cf34e7a1330517cbc91
eighthundrednine <wdkevo@gmail.com>
2022-04-09 22:11:14 -0400

    Completed proof of concept

---
+++ pages/script.js
@@ -0,0 +1,5 @@
+// import particlesJS from "./particles";
+
+// particlesJS.load("particles-js", "./particle-config.json", function () {
+//   console.log("callback - particles.js config loaded");
+// });
