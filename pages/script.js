17abbc8f707ad64de60253c82f65380ef67d1c02
eighthundrednine <wdkevo@gmail.com>
2022-04-09 22:24:01 -0400

    remove unused files

--- pages/script.js
+++
@@ -1,5 +0,0 @@
-// import particlesJS from "./particles";
-
-// particlesJS.load("particles-js", "./particle-config.json", function () {
-//   console.log("callback - particles.js config loaded");
-// });
